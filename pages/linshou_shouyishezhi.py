382535b6aa632ed15faeeeec5347433f4f48c5d1
jiao <137501797@qq.com>
2019-05-10 18:25:13 +0800

    jiaoziyi

--- pages/linshou_shouyishezhi.py
+++ pages/linshou_shouyishezhi.py
@@ -5,5 +5,5 @@ from selenium.webdriver.common.by import By
 class Linshou:
     linshoutab = (By.XPATH, "//*[@role='menuitem']/span[text()='零售']")
     tianjiachanpinbutton =(By.XPATH, "//*[text()='添加产品' and @onclick='getpage(1)']")
-
+    tianjiachanpinbutton1 =(By.XPATH, "//*[text()='添加产品' and @onclick='getpage(1)']")
 
